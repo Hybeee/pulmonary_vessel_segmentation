2a6417622a542fe31963bc9acc6f2a0819408341
Hybeee <szcsa2003@gmail.com>
2025-04-19 14:25:37 +0200

    ts_util.py created. Contains code that utilizes totalsegmentator for different tasks. Currently one task is added: returning pulmonary binary mask from an input ct scan

---
+++ code/utils/ts_util.py
@@ -0,0 +1,20 @@
+from totalsegmentator.python_api import totalsegmentator
+import numpy as np
+
+def extract_binary_mask(mask, class_indices):
+    """
+    Converts a multi-class mask into a binary mask for the specified class index or indices.
+    """
+    if class_indices is not np.array:
+        class_indices = np.array(class_indices)
+    
+    binary_mask = (np.isin(mask, class_indices)).astype(int)
+    return binary_mask
+
+def get_pulmonary_mask(ct):
+    """
+    Runs the 'total' segmentation task from TS on the given CT scan,
+    and returns a binary mask corresponding to class index 53 (pulmonary vein).
+    """
+    output_mask = totalsegmentator(ct, task='total')
+    return extract_binary_mask(output_mask, [53])
