f5bc7cac80a89aae61d5c7f0d49156923d7c774c
Hybeee <szcsa2003@gmail.com>
2025-04-29 23:52:43 +0200

    Minor adjustments to traverse_component's edge traversal implementation

--- code/experiments/skeleton_traverser.py
+++ code/experiments/skeleton_traverser.py
@@ -61,7 +61,7 @@ def traverse_component(intersection_point, graph, visited_nodes, endpoints, edge
     current_segment_start = None
     current_segment_end = None
     current_segment_index = edge_segment_start_index
-    # remaining_step_size = 0 # NOTE: is this needed? If not handled different step_sizes might occur if edge_length % step_size != 0
+    remaining_step_size = 0 # NOTE: is this needed? If not handled different step_sizes might occur if edge_length % step_size != 0
 
     future_nodes = [] # when reaching next node: .append(list(graph[next_node].keys())) BEFORE setting next_node as prev node
     visited_nodes.append(prev_node) # in reality prev node is never visited - used so that traversal doesn't continue this way.
@@ -73,7 +73,6 @@ def traverse_component(intersection_point, graph, visited_nodes, endpoints, edge
             current_segment_end = current_edge[current_segment_index + 1]
 
         # TRAVERSING EDGE BETWEEN PREV_NODE AND NEXT_NODE
-        remaining_step_size = 0
         while current_segment_index < len(current_edge) - 1: # current_edge[len(current_edge) - 2] <=> -2nd element. -> the starting point of the last edge.
             current_segment_start = current_edge[current_segment_index]
             current_segment_end = current_edge[current_segment_index + 1]
@@ -81,6 +80,11 @@ def traverse_component(intersection_point, graph, visited_nodes, endpoints, edge
             segment_vector = current_segment_end - current_segment_start
             segment_length = get_distance(current_segment_start, current_segment_end)
             segment_unit_vector = segment_vector / segment_length
+
+            # just to make sure
+            if segment_length == 0:
+                current_segment_index += 1
+                continue
 
             if remaining_step_size > 0:
                 if remaining_step_size > segment_length:
@@ -93,7 +97,7 @@ def traverse_component(intersection_point, graph, visited_nodes, endpoints, edge
                 remaining_length = get_distance(current_position, current_segment_end)
                 if step_size > remaining_length:
                     current_segment_index += 1
-                    remaining_step_size = step_size - segment_length
+                    remaining_step_size = step_size - remaining_length
                 else:
                     current_position += segment_unit_vector * step_size
 
