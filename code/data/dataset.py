05d79288fd3ffa2d2f18d07974f86e968210b963
Hybeee <szcsa2003@gmail.com>
2025-05-20 22:31:17 +0200

    Init written for IterativeSegmentationDataset

--- code/data/dataset.py
+++ code/data/dataset.py
@@ -1,1 +1,20 @@
 from torch.utils.data import Dataset
+from data_preparer import DataPreparer
+
+
+class IterativeSegmentationDataset(Dataset):
+    def __init__(self, dataPreparer: DataPreparer, transform):
+        super(IterativeSegmentationDataset, self).__init__()
+        self.cts = dataPreparer.cts
+
+        self.artery_masks = dataPreparer.artery_masks
+        self.artery_skeletons = dataPreparer.artery_skeletons
+        self.traversed_artery_paths = dataPreparer.traversed_artery_paths
+
+        self.vein_masks = dataPreparer.vein_masks
+        self.vein_skeletons = dataPreparer.vein_skeletons
+        self.traversed_vein_paths = dataPreparer.traversed_vein_paths
+
+        self.spacings = dataPreparer.spacings
+
+        self.transform = transform
