67eaa74164377b51d86359b76572296cb7031cd6
Hybeee <szcsa2003@gmail.com>
2025-05-20 21:47:41 +0200

    Other refactoring, improved project/code organization

--- code/utils/ts_util.py
+++ code/utils/ts_util.py
@@ -8,7 +8,7 @@ def extract_binary_mask(class_indices):
     Converts a multi-class mask into a binary mask for the specified class index or indices.
     CURRENTLY NOT NEEDED, FOUND AN EASIER WAY TO MAKE THIS WORK!
     """
-    seg_nifti_img, _ = load_multilabel_nifti("code/temp_images/temp_ts/output_mask.nii")
+    seg_nifti_img, _ = load_multilabel_nifti("code/resources/temp_images/temp_ts/output_mask.nii")
     mask = seg_nifti_img.get_fdata()
 
     if not isinstance(class_indices, np.ndarray):
@@ -24,10 +24,10 @@ def get_pulmonary_mask(ct):
     """
     print("Generating pulmonary_mask...")
     ct_sitk = sitk.GetImageFromArray(ct)
-    ct_path = "code/temp_images/temp_ct.nii.gz"
+    ct_path = "code/resources/temp_images/temp_ct.nii.gz"
     sitk.WriteImage(ct_sitk, ct_path)
 
-    output_mask = totalsegmentator(ct_path, task='total', output="code/temp_images/temp_ts_output")
-    pulmonary_mask = sitk.ReadImage("code/temp_images/temp_ts_output/pulmonary_vein.nii.gz")
+    output_mask = totalsegmentator(ct_path, task='total', output="code/resources/temp_images/temp_ts_output")
+    pulmonary_mask = sitk.ReadImage("code/resources/temp_images/temp_ts_output/pulmonary_vein.nii.gz")
     pulmonary_mask = sitk.GetArrayFromImage(pulmonary_mask)
     return pulmonary_mask
